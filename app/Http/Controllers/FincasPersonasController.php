f83ac2e8a5cacf330fcb8bd4b6cf9b0327cd33c5
Yesid Alejandro Sanchez Diaz <yesid.alejo300@gmail.com>
2016-08-22 17:24:04 -0500

    Add files via upload

--- app/Http/Controllers/FincasPersonasController.php
+++ app/Http/Controllers/FincasPersonasController.php
@@ -8,28 +8,23 @@ use produccion\Http\Requests;
 
 class FincasPersonasController extends ClienteController
 {
-    //mostrar las personas y sus fincas
-	//Yesid falta hacer este link es muy parecido a consultar siembra por persona
 	  public function mostrarPersonas()
     {
     	$personas = $this->obtenerTodasLasPersonas();
     	return view('persona-fincas.personas', ['personas' => $personas]);
-    }
-	
-	 //mostrar las personas y sus fincas
-	//Yesid falta hacer este link es muy parecido a consultar siembra por persona
+	}
     public function mostrarFincasPersona(Request $request)
     {
     	$fincaId = $request->get('persona_id');
     	$fincas = $this->obtenerFincasPersona($fincaId);
-    	return view('persona-siembras.fincas', ['fincas' => $fincas]);
-    }
-	
-	//métodos para agregar fincas
-    public function agregarFinca()
+    	return view('persona-siembras.finca', ['fincas' => $fincas]);	
+	}
+//Agregar finca
+//1. Formulario para agregar una finca cargando las personas que ya están en la basede datos
+  public function agregarFinca()
     {
 		 $personas = $this->obtenerTodasLasPersonas();
-		return view('persona-siembras.agregar', ['personas' => $personas]);
+		return view('persona-fincas.agregar', ['personas' => $personas]);
     }
 	
  
@@ -39,41 +34,49 @@ class FincasPersonasController extends ClienteController
         $this->almacenarFinca($request);
         return redirect('/fincas');
     }
+
+//Eliminar una finca
+//1.
 	
-		
-	//Métodos para editar Fincas
-    //1. 
-    public function elegirFinca()
+	 public function seleccionarFinca()
     {
         $fincas = $this->obtenerTodasLasFincas();
-        return view('fincas.elegir', ['finca' => $fincas]);
-    }
-	//.2
-	public function editarFinca(Request $request)
-    {
-        $id = $request->get('finca_id');
-        $cosecha = $this->obtenerUnaFinca($id);
-        return view('fincas.editar', ['finca' => $finca]);
-    }
 	
-	//3.
-    public function actualizarFinca(Request $request)
-    {
-        $this->modificarFinca($request);
-        return redirect('/fincas');
+        return view('persona-fincas.seleccionar', ['fincas' => $fincas]);
     }
-	
-	//Método para Eliminar fincas
-	
-		    public function seleccionarFinca()
-    {
-        $fincas = $this->obtenerTodasLasFincas();
-        return view('fincas.seleccionar', ['fincas' => $fincas]);
-    }
-	
+//2.
     public function eliminarFinca(Request $request)
     {
         $this->removerFinca($request);
         return redirect('/fincas');
     }
+
+//actualizar fincas
+//1.
+
+public function elegirFinca()
+    {
+        $fincas = $this->obtenerTodasLasFincas();
+
+        return view('fincas.elegir', ['fincas' => $fincas]);
+    }
+//2. Editar finca
+
+    public function editarFinca(Request $request)
+    {
+        $idFinca = $request->get('finca_id');
+
+        $finca = $this->obtenerUnaFinca($idFinca);
+   		
+        $personas = $this->obtenerTodasLasPersonas();
+
+        return view('persona-fincas.editar', ['siembra' => $finca, 'personas' => $personas]);
+    }
+
+    public function actualizarFinca(Request $request)
+    {
+        $this->modificarFinca($request);
+
+        return redirect('/Fincas');
+	}
 }
