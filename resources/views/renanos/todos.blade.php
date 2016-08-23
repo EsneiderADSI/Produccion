6c85251f818cf6b625315f3456468b9b9fbd2b08
Yesid Alejandro Sanchez Diaz <yesid.alejo300@gmail.com>
2016-08-23 16:15:52 -0500

    Add files via upload

---
+++ resources/views/renanos/todos.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+@section('contenido')
+	@if(sizeof($renanos) > 0)
+
+		<table class="table table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>ano_ren </th>
+                    <th>proreal_ren </th>
+					<th>proesparada_ren	 </th>
+					<th>siembra_id </th>
+					
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($finca as $finca)
+				<tr>
+					<td>{{$renanos->id}}</td>
+                    <td>{{$renanos->ano_ren}}</td>
+                    <td>{{$renanos->proreal_ren}}</td>
+                    <td>{{$renanos->proesparada_ren}}</td>
+                    <td>{{$renanos->siembra_id}}</td>
+				
+				
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+		@else
+
+		<div class="alert alert-danger">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+			<strong>Ups</strong> No hay años de produccion 
+		</div>
+
+		@endif
+@endsection
